01cf93fdea4c6868f55ab34382210efd36c1b8dd
heinold <m.heinold@dkfz-heidelberg.de>
2019-09-17 15:16:35 +0200

    Add a message when a test was started / finished
    I had the problem, that a failure in my application lead to a total
    crash of the test system. Unfortunately, UT++ did not show any location
    of the error or the started test. What I did was to output, e.g.:
    
      Executing test: [68] 'IOHELPER_TESTS'::'fullPath'
       when a test was started. This shows a number, the suite and the test id.
    
    and
    
      .. Finished [68]
       when the test finished.
    
    This way, I can at least determine the latest test in use.

--- UnitTest++/Test.cpp
+++ UnitTest++/Test.cpp
@@ -5,34 +5,42 @@
 #include "AssertException.h"
 #include "MemoryOutStream.h"
 #include "ExecuteTest.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstring>
+#include <iostream>
 
 #ifdef UNITTEST_POSIX
+
 #include "Posix/SignalTranslator.h"
+
 #endif
+
+long id=0;
 
 namespace UnitTest {
 
-   TestList& Test::GetTestList()
-   {
-      static TestList s_list;
-      return s_list;
-   }
+    using namespace std;
 
-   Test::Test(char const* testName, char const* suiteName, char const* filename, int lineNumber)
-      : m_details(testName, suiteName, filename, lineNumber)
-      , m_nextTest(0)
-      , m_isMockTest(false)
-   {}
+    TestList &Test::GetTestList() {
+        static TestList s_list;
+        return s_list;
+    }
 
-   Test::~Test()
-   {}
+    Test::Test(char const *testName, char const *suiteName, char const *filename, int lineNumber)
+            : m_details(testName, suiteName, filename, lineNumber), m_nextTest(0), m_isMockTest(false) {}
 
-   void Test::Run()
-   {
-      ExecuteTest(*this, m_details, m_isMockTest);
-   }
+    Test::~Test() {}
 
-   void Test::RunImpl() const
-   {}
+    void Test::Run() {
+        long _id = id++;
+        std::cerr << "Executing test: [" << _id << "] '" << this->m_details.suiteName << "'::'" << this->m_details.testName << "'\n";
+//        std::cerr << "  Next test would be: '" << this->m_nextTest->m_details.suiteName << "', '" << this->m_nextTest->m_details.testName << "', '" << "'\n";
+        ExecuteTest(*this, m_details, m_isMockTest);
+        std::cerr << ".. Finished [" << _id << "]\n";
+    }
+
+    void Test::RunImpl() const {}
 
 }
